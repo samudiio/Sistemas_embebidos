7392be1119045f063f0ea7cf57a9de9ae8e8df16
ManuelNorza <espinorza@hotmail.com>
2017-10-25 19:24:45 -0500

    Prueba Con EnableInt

--- BaseProject/source/bsw/mcal/uart/Uart_Cfg.c
+++ BaseProject/source/bsw/mcal/uart/Uart_Cfg.c
@@ -10,15 +10,12 @@ Configuration of the UART (Uart driver) module
 void FnTxNotification(void){
     printf("Enviado \n\r");
     Uart_EnableInt(0, 2, 0);
-    Uart_EnableInt(0, 1, 1);
 }
 
 void FnRxNotification(void){
     uint8_t dta;
     Uart_GetByte(0, &dta);
     printf("Recibido: %c\n\r", dta);
-    Uart_EnableInt(0, 1, 0);
-    Uart_EnableInt(0, 2, 1);
 }
 
 void FnErrNotification(UartErrorType Error){
