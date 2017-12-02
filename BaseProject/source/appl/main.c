ad7808c0b4522314efb77a04b03693888bb06fca
ManuelNorza <espinorza@hotmail.com>
2017-12-02 11:04:20 -0600

    Se agrega Fft

--- BaseProject/source/appl/main.c
+++ BaseProject/source/appl/main.c
@@ -9,11 +9,15 @@
 #include "Afec_Ctrl.h"
 #include <stdbool.h>
 #include <stdio.h>
+/**  Fast fourier transform */
+#include    "fft.h"
+#include    "ecg_data.h"
 
 /*----------------------------------------------------------------------------
  *        Local definitions
  *----------------------------------------------------------------------------*/
 
+#define TEST_LENGTH_SAMPLES 2048
 
 
 TaskType Tasks[]={
@@ -44,6 +48,17 @@ static void _ConfigureLeds( void )
 	LED_Configure( 1 ) ;
 }
 
+/*~~~~~~  Global variables ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+/** Auxiliary input buffer to accomodate data as FFT function expects it */
+float       fft_inputData[TEST_LENGTH_SAMPLES];
+/** Output magnitude data */
+float       fft_signalPower[TEST_LENGTH_SAMPLES/2];
+/** Auxiliary output variable that holds the frequency bin with the highest level of signal power */
+uint32_t    u32fft_maxPowerIndex;
+/** Auxiliary output variable that holds the maximum level of signal power */
+float       fft_maxPower;
+
 /*----------------------------------------------------------------------------
  *        Exported functions
  *----------------------------------------------------------------------------*/
@@ -58,7 +73,8 @@ uint32_t SAMP_PER = 50;
 
 extern int main( void )
 {
-
+	/** Auxiliary array index */
+    uint16_t    u16index;
 	/* Disable watchdog */
 	WDT_Disable( WDT ) ;
 
@@ -80,6 +96,7 @@ extern int main( void )
 	vfnScheduler_Init(&Tasks[0]);
 	/* Start execution of task scheduler */
 	vfnScheduler_Start();
+	vfnFpu_enable();
   
   //printf( "size of uint8_t = %d.\n\r", sizeof(uint8_t));
   
@@ -98,6 +115,18 @@ extern int main( void )
 	/*-- Loop through all the periodic tasks from Task Scheduler --*/
 	for(;;)
 	{
+		/*Prepare data for FFT operation */
+        for (u16index = 0; u16index < (TEST_LENGTH_SAMPLES/2); u16index++)
+        {
+            fft_inputData[(2*u16index)] = ecg_resampled[u16index];
+            fft_inputData[(2*u16index) + 1] = 0;
+        }
+        /** Perform FFT on the input signal */
+        fft(fft_inputData, fft_signalPower, TEST_LENGTH_SAMPLES/2, &u32fft_maxPowerIndex, &fft_maxPower);
+        
+        /* Publish through emulated Serial the byte that was previously sent through the regular Serial channel */
+		printf("%5d  %5.4f \r\n", u32fft_maxPowerIndex, fft_maxPower);
+		
 		/* Perform all scheduled tasks */    
 		vfnTask_Scheduler();
 	}
